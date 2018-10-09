9f7ef142ff4e6ec1e089d96ad56542458faad21c
Bimo Bramantyo <bim.bramantyo@gmail.com>
2018-10-09 17:49:51 +0700

    It should have a way to display todos

---
+++ 03_Version_1_Arrays/02_It_should_have_a_way_to_display_todos/code.js
@@ -0,0 +1,8 @@
+// We have a place to store our todo
+var todos = ['item1', 'item2', 'item3'];
+
+console.log("hello there");
+
+console.log("hello there", "gordon");
+
+console.log("My todos", todos);
